10c69dbcc9fc366367e97117c6cffb7b5b76c8fd
ChriD <chris_d85@hotmail.com>
2016-10-07 18:26:17 +0200

    1.0.1 - Added debug option for better crash info

--- source/buildX86.sh
+++ source/buildX86.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-make arch=X86 clean
-make arch=X86
+make arch=X86 dbg=1 clean
+make arch=X86 dbg=1
 /bin/cp -rf libs/_tmpBuild/raumkernel.so libs/linux_x86/raumkernel.so
-make arch=X86 clean
+make arch=X86 dbg=1 clean
